95a4fe2b9a16d47ef4fff869c26655cbabb3f3ea
haruki1234 <79097169+haruki1234@users.noreply.github.com>
2021-12-04 16:17:13 +0900

    プロジェクト ファイルを追加します。

---
+++ english_typing/TitlePage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace english_typing
+{
+    /// <summary>
+    /// Page2.xaml の相互作用ロジック
+    /// </summary>
+    public partial class TitlePage : Page
+    {
+        bool cpage = false;
+
+        public TitlePage()
+        {
+            InitializeComponent();
+            ProgressBar.IsIndeterminate = true;
+
+            DispatcherTimer timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(5) };
+            timer.Start();
+            timer.Tick += (s, args) =>
+            {
+                // タイマーの停止
+                timer.Stop();
+                ProgressBar.IsIndeterminate = false;
+
+                // 以下に待機後の処理を書く
+                if (cpage == false)
+                {
+                    var tpage = new HomePage();
+                    NavigationService.Navigate(tpage);
+                }
+            };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            cpage = true;
+            var tpage = new HomePage();
+            NavigationService.Navigate(tpage);
+        }
+    }
+}
